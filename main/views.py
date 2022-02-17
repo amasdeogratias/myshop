2358dee62f0ac0f701313fe6b39e8b283e413647
amasdeogratias <deogratiasprog@gmail.com>
2022-02-17 15:41:28 +0300

    Display carts and checkout data

--- main/views.py
+++ main/views.py
@@ -8,9 +8,25 @@ def store(request):
     return render(request, 'main/store.html', context)
 
 def cart(request):
-    context = {}
+    # check if user is authenticated
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer = customer, complete = False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+    context = {'items':items, 'order':order}
     return render(request, 'main/cart.html', context)
 
 def checkout(request):
-    context = {}
+    # check if user is authenticated
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer = customer, complete = False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+    context = {'items':items, 'order':order}    
     return render(request, 'main/checkout.html', context)
