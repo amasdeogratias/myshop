853050d31dff179f3e156e8ce32b4dcdffbc011e
amasdeogratias <deogratiasprog@gmail.com>
2022-02-19 11:13:28 +0300

    Adding shipping and complete the payment for logged in user

--- main/views.py
+++ main/views.py
@@ -1,8 +1,10 @@
 import json
 from django.shortcuts import render
 from django.http import JsonResponse
-from .models import *
 import json
+import datetime
+from .models import *
+
 
 # Create your views here.
 def store(request):
@@ -34,6 +36,8 @@ def cart(request):
         cartItems = order['get_cart_items']
     context = {'items':items, 'order':order, 'cartItems':cartItems}
     return render(request, 'main/cart.html', context)
+
+
 
 def checkout(request):
     # check if user is authenticated
@@ -72,6 +76,37 @@ def updateItem(request):
     if orderItem.quantity <= 0:
         orderItem.delete()
 
-
     return JsonResponse('Item was added', safe=False)
+
+from django.views.decorators.csrf import csrf_exempt
+
+csrf_exempt
+def processOrder(request):
+    transaction_id = datetime.datetime.now().timestamp()
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        data = json.loads(request.body)
+        order, created = Order.objects.get_or_create(customer = customer, complete = False)
+        total = float(data['form']['total'])
+        order.transaction_id = transaction_id
+
+        if total == order.get_cart_total:
+            order.complete = True
+        order.save()
+
+        if order.shipping == True:
+            ShippingAddress.objects.create(
+                customer = customer,
+                order = order,
+                address = data['shipping']['address'],
+                city = data['shipping']['city'],
+                state = data['shipping']['state'],
+                zipcode = data['shipping']['zipcode'],
+            )
+    else:
+        print('User is not logged in')
+    return JsonResponse('Payment Completed', safe=False)
+
+    
     
