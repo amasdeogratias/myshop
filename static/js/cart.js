62764b5995a0a03610a4ae7afcb3b3adf6f2111e
amasdeogratias <deogratiasprog@gmail.com>
2022-03-31 11:05:43 +0300

    Setting up the cookies

--- static/js/cart.js
+++ static/js/cart.js
@@ -7,13 +7,40 @@ for (var i = 0; i < updateBtns.length; i++) {
         console.log('productId:', productId, 'action:', action)
 
         console.log('USER:', user)
-        if (user === 'AnonymousUser') {
-            console.log('not logged in')
+        if (user == 'AnonymousUser') {
+            addCookieItem(productId, action);
         } else {
             updateUserOrder(productId, action)
         }
     })
 }
+
+function addCookieItem(productId, action) {
+    console.log('User is not authenticated...');
+
+    if (action == 'add') {
+        if (cart[productId] == undefined) { //if item is not in cart
+            cart[productId] = { 'quantity': 1 }
+
+        } else {
+            cart[productId]['quantity'] += 1
+        }
+    }
+
+    if (action == 'remove') {
+        cart[productId]['quantity'] -= 1
+
+        if (cart[productId]['quantity'] <= 0) {
+            delete cart[productId]
+        }
+
+    }
+    console.log('Cart:', cart);
+    document.cookie = 'cart=' + JSON.stringify(cart) + "; domain=;path=/"
+    location.reload()
+
+}
+
 
 function updateUserOrder(productId, action) {
     console.log(('User is logged in, sending data..'))
